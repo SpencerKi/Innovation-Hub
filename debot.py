4cf112a74ebeef5a5e43a0cb94e8715debb6f861
Spencer Ki <65224711+SpencerKi@users.noreply.github.com>
2022-11-03 22:35:47 -0400

    Add files via upload

---
+++ debot.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Spencer Y. Ki
+2022-11-02
+
+why_am_i_doing_this.py
+"""
+import numpy as np
+import scipy as sp
+import pandas as pd
+
+# Data import
+raw_dat = pd.read_excel("qap.xlsx")
+dat = raw_dat.to_numpy()
+
+# Conditionals
+time_min = 600 # in seconds
+short_qs = np.array(["DJ","DS"]) # columns in excel
+
+# Helper functions that really should be in a module
+def column_converter(col):
+    """
+    Converts excel column to usable indices.
+    """
+    num = 0
+    for c in col:
+        if isinstance(c, str):
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num - 1
+
+def short_checker(response: str):
+    """
+    Checks short answers against criteria
+    """
+    if not isinstance(response, str): # removes blanks
+        return False
+    elif len(response) == 1: # removes single character responses
+        return False
+    return True
+    
+# P U R G E
+bad = []
+for i in dat:    
+    # Time-based criteria
+    if (i[2]-i[1]).total_seconds() < time_min:
+        bad.append(i[0]-1)
+        continue
+    
+    # Short-response criteria
+    for j in short_qs:
+        if not short_checker(i[column_converter(j)]):
+            bad.append(i[0]-1)
+
+results = np.delete(dat, np.array(bad), 0)
+df_results = pd.DataFrame(data=results[0:,1:], index=results[0:,0], columns=np.array(raw_dat.columns)[1:])
+df_results.to_excel("output.xlsx")
