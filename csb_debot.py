5d3c1dfb6e385422c23faf9c37ecf6389738d8f0
Spencer Ki <65224711+SpencerKi@users.noreply.github.com>
2022-11-24 19:16:18 -0500

    Update csb_debot.py
    Moved helper functions to separate module. Probably last change to this program before deprecation.

--- csb_debot.py
+++ csb_debot.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 Spencer Y. Ki
-2022-11-17
+2022-11-23
 
 Script for de-botting Cell & Systems Biology UTQAP survey data.
 Written for use by the UofT Innovation Hub.
 """
-# Reassuring message for the executable
-print("Hello! This may take up to a minute. Please stand by.")
-
 # Why can't numpy directly read excel files...
 import numpy as np
 import pandas as pd
+import debot_helpers as dh
 from collections import Counter
 from time import sleep
 from sys import exit
+
+# Reassuring message
+print("Hello! This may take up to a minute. Please stand by.")
 
 try:
     # Data import and conversion to numpy array
@@ -29,69 +30,20 @@ except:
     sleep(10)
     exit()
 
-# Assorted helper functions that really should be in a module
-def col_conv(col: str) -> float:
-    """Converts excel column to usable indices.
-    col: Alphabetic column identifier (e.g., "A" for the first column)
-    return: Python-usable index
-    
-    >>> col_conv("A")
-    0
-    >>> col_conv("AB")
-    27
-    """
-    num = 0
-    for c in col:
-        num = num * 26 + (ord(c.upper()) - ord('A')) + 1# Based on Unicode IDs
-    return num - 1
-
-def student_status(student: np.ndarray) -> str:
-    """Checks if a student responded to the grad and/or undergrad questions.
-    student: Array of a respondent's answers to the survey
-    return: String indicating student status
-    
-    Test cases vary by data.
-    """
-    # Questions I and L ask for level of study for current students and alumni
-    if isinstance(student[col_conv("I")], str) \
-    or isinstance(student[col_conv("L")], str):
-        if student[col_conv("I")] == "Graduate" \
-        or student[col_conv("L")] == "Graduate":
-            # Question CI asks if they also completed undergrad at UofT
-            if student[col_conv("CI")] == "Yes":
-                return "Both"
-            elif student[col_conv("CI")] == "No":
-                return "Graduate"
-            else:
-                return "Missing"
-        elif student[col_conv("I")] == "Undergraduate" \
-        or student[col_conv("L")] == "Undergraduate":
-            return "Undergraduate"
-        else:
-            return "Missing"
-    else:
-        return "Missing"
-
-def fetch_stats(df: pd.DataFrame, q: str) -> pd.core.series.Series:
-    """Gives statistical overview of responses to one question.
-    df: DataFrame of results (e.g., uncleaned dat or cleaned df_results)
-    q: Alphabetic column identifier for the desired question
-    
-    This is here for diagnostic and is unused by the main script.
-    """
-    return df.iloc()[:,col_conv(q)].describe()
-
 # Conditionals, grouped for easy editing
 complete_min = 360# Desired completion time minimum in seconds
 simple_min = 3# Max length for simple responses (e.g., "no" or "n/a")
 grad_short_qs = np.array(["CE","CH"])# Question columns in excel
 ug_short_qs = np.array(["CZ","DJ","DS","EJ"])
+current = "I"# Column with current student identifier
+alum = "L"# Column with alumni identifier
+both = "CI"# Column with 'double alumni' identifier
 
 # Identifying bad responses
 all_responses = []
 # This loop grabs all the short responses in the whole dataset
 for i in np.concatenate((grad_short_qs, ug_short_qs)):
-    all_responses = np.concatenate((all_responses, dat.T[col_conv(i)]))
+    all_responses = np.concatenate((all_responses, dat.T[dh.col_conv(i)]))
 bad_responses = []
 # This loop identifies which of the responses are undesirable
 # - First condition removes duplicates over specified length
@@ -117,14 +69,14 @@ for i in dat:
     # if conditions check for student status
     # First for loop in each condition checks which questions should be checked
     # Second for loop checks if any responses are on the bad list
-    stat = student_status(i)
+    stat = dh.student_status(i, current, alum, both, True)
     resp = []
     if stat == "Missing":
         bad.append(i[0]-1)
         continue
     elif stat == "Graduate":
         for j in grad_short_qs:
-            resp.append(i[col_conv(j)])
+            resp.append(i[dh.col_conv(j)])
         for k in list(Counter(resp).items()):
             if k[0] in bad_responses:
                 bad.append(i[0]-1)
@@ -132,7 +84,7 @@ for i in dat:
         continue
     elif stat == "Undergraduate":
         for j in ug_short_qs:
-            resp.append(i[col_conv(j)])
+            resp.append(i[dh.col_conv(j)])
         for k in list(Counter(resp).items()):
             if k[0] in bad_responses:
                 bad.append(i[0]-1)
@@ -143,7 +95,7 @@ for i in dat:
             bad.append(i[0]-1)
             continue
         for j in np.concatenate((grad_short_qs, ug_short_qs)):
-            resp.append(i[col_conv(j)])
+            resp.append(i[dh.col_conv(j)])
         for k in list(Counter(resp).items()):
             if k[0] in bad_responses:
                 bad.append(i[0]-1)
